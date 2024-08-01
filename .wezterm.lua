01d00048336258286ba82c800bedbf31f0ffb9f0
Emamul Andalib <emamul.andalib@gmail.com>
2024-08-01 22:13:15 +0200

    Wezterm
    - font changed

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("FiraCode Nerd Font Mono")
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 15
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
