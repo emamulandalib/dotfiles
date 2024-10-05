b40ae3516737e9dc29a1046dabc2b84290474665
Emamul Andalib <emamul.andalib@gmail.com>
2024-10-05 23:32:56 +0200

    TOO Much Font Changes!

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,15 +2,14 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("Maple Mono")
--- config.font = wezterm.font_with_fallback({
--- 	{
--- 		family = "Monaspace Neon",
--- 		{ weight = 'Bold', italic = true },
--- 		harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "dlig" },
--- 	},
--- })
-config.font_size = 16.5
+-- config.font = wezterm.font("Maple Mono")
+config.font = wezterm.font_with_fallback({
+	{
+		family = "Monaspace Neon",
+		harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "dlig" },
+	},
+})
+config.font_size = 16
 config.enable_tab_bar = false
 config.initial_cols = 110
 config.initial_rows = 30
