98389913ab3ef4dee03bfc76b452f6b728b54b59
Emamul Andalib <emamul.andalib@gmail.com>
2024-10-06 22:34:17 +0200

    TOO Much Font Changes!

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- config.font = wezterm.font("Maple Mono", { weight = "Medium", italic = false })
+-- config.font = wezterm.font("Monaspace Neon", { weight = "200", width = "100", italic = false })
 config.font = wezterm.font("MonoLisa Trial", {
 	weight = "Medium",
 	italic = false,
@@ -14,7 +15,7 @@ config.font = wezterm.font("MonoLisa Trial", {
 -- 		harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "dlig" },
 -- 	},
 -- })
-config.font_size = 15
+config.font_size = 16.5
 config.enable_tab_bar = false
 config.initial_cols = 110
 config.initial_rows = 30
