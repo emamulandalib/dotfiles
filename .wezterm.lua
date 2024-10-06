a2d2e3b108a78c5a01129e16b7a8e80f50435d25
Emamul Andalib <emamul.andalib@gmail.com>
2024-10-06 23:09:29 +0200

    TOO Much Font Changes!

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,19 +2,26 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
--- config.font = wezterm.font("Maple Mono", { weight = "Medium", italic = false })
--- config.font = wezterm.font("Monaspace Neon", { weight = "200", width = "100", italic = false })
+-- config.font = wezterm.font("Maple Mono", {
+-- 	weight = "Medium",
+-- 	italic = false,
+-- 	harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "dlig" },
+-- })
+-- config.font = wezterm.font(
+-- 	"Monaspace Neon",
+-- 	{
+-- 		weight = "200",
+-- 		width = "100",
+-- 		italic = false,
+-- 		harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "dlig" },
+-- 	}
+-- )
 config.font = wezterm.font("MonoLisa Trial", {
 	weight = "Medium",
 	italic = false,
 	harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "dlig" },
 })
--- config.font = wezterm.font_with_fallback({
--- 	{
--- 		family = "Monaspace Neon",
--- 		harfbuzz_features = { "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08", "calt", "dlig" },
--- 	},
--- })
+
 config.font_size = 16
 config.enable_tab_bar = false
 config.initial_cols = 110
